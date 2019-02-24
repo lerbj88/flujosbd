57b615585c2db58125a2332ddfaf7c5a01351df3
benja <lerbj88@gmail.com>
2019-02-23 21:03:01 -0600

    inicio

---
+++ src/main/java/com/flujos/flujosbd/config/KondigurasiSecurity.java
@@ -0,0 +1,70 @@
+package com.flujos.flujosbd.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableWebSecurity
+public class KondigurasiSecurity extends WebSecurityConfigurerAdapter{
+    public static final String SQL_LOGIN
+
+            ="select fiusuario, fcpassword, active\n" +
+            "from usuarios where fiusuario = ?";
+
+    public static final String SQL_PERMISSION
+            = "select u.fiusuario, r.fcrol as authority\n" +
+            "from usuarios u join usuarios_roles ur on u.fiid = ur.fiidusuario\n" +
+            "join roles r on ur.fiidrol = r.fiid\n" +
+            "where u.fiusuario = ?";
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configurGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(SQL_LOGIN)
+                .authoritiesByUsernameQuery(SQL_PERMISSION)
+                .passwordEncoder(passwordEncoder()); // bcrypt
+
+
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+
+                .authorizeRequests()
+
+
+                .antMatchers("/static/js/js/**").permitAll()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/fonts/**").permitAll()
+
+
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+               .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/usuarios/list", true)
+                .and()
+                .logout();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ src/main/java/com/flujos/flujosbd/controller/UsuarioController.java
@@ -0,0 +1,75 @@
+package com.flujos.flujosbd.controller;
+
+
+import com.flujos.flujosbd.dao.UsuariosDao;
+import com.flujos.flujosbd.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Controller
+public class UsuarioController {
+
+    @Autowired
+    private UsuariosDao usuariosDao;
+/*
+    @RequestMapping(value = { "usuarios/list" }, method = RequestMethod.GET)
+    public String listarUsuarios(Model model) throws SQLException {
+
+        List<String> list = usuariosDao.listarUsuarios();
+        model.addAttribute("listarUsuarios", list);
+
+        return "usuarios/list";
+    }
+
+*/
+
+    @RequestMapping(value = { "/" }, method = RequestMethod.GET)
+    public String home(Model model) throws SQLException {
+
+        return "home";
+    }
+
+    @RequestMapping(value = { "usuarios/list" }, method = RequestMethod.GET)
+    public String listarUsuarios(Model model) throws SQLException {
+
+        List<Usuario> list = usuariosDao.findAll();
+        model.addAttribute("listarUsuarios", list);
+
+        return "usuarios/list";
+    }
+
+
+    @GetMapping("usuarios/form")
+    public String form(ModelMap model){
+        Usuario usuario = new Usuario();
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("outMessage", "");
+        return "usuarios/form";
+    }
+
+
+    @PostMapping("/form")
+    public String agregar(@ModelAttribute(value = "usuario") Usuario usuario, BindingResult errors, SessionStatus status, ModelMap model) {
+        if (errors.hasErrors()) {
+            return "usuario/form";
+        }
+
+
+        status.setComplete();
+        //usuario.crearUsuario(usuario.getUsuario(), usuario.getPassword());
+        usuario.crearUsuario(usuario.getUsuario(), usuario.getPassword());
+        model.addAttribute("crearusuario", usuario);
+
+        return "usuario/form";
+    }
+
+}
